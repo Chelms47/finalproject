cfa89891408bdc7afc3a974b1049f468ded3e171
chanda <helmschanda@gmail.com>
2018-11-14 18:00:45 -0500

    uploading project file

---
+++ README.md
@@ -0,0 +1,3 @@
+# project2
+Project for Code Louisville Front-End
+# finalproject
---
+++ sam.js
@@ -0,0 +1,7 @@
+var email = prompt("Please enter your email ro recieve newsletter", );
+
+if (email == null || email == "") {
+    txt = "No Thank you";
+} else {
+    txt = "newsletters will be sent to " + person + "! Thanks for staying in touch";
+}
