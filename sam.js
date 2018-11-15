0ffcfd863867fb01b951e361b65e33663ddb051d
chanda <helmschanda@gmail.com>
2018-11-14 19:19:24 -0500

    updated form

--- sam.js
+++ sam.js
@@ -1,7 +1,7 @@
-var email = prompt("Please enter your email ro recieve newsletter", );
-
-if (email == null || email == "") {
-    txt = "No Thank you";
-} else {
-    txt = "newsletters will be sent to " + person + "! Thanks for staying in touch";
+function myFunction() {
+    var person = prompt("Please enter your email", "you@domain.com");
+    if (person != null) {
+        document.getElementById("news").innerHTML =
+        "newsletter will be sent to " + person + "! thank you!";
+    }
 }
